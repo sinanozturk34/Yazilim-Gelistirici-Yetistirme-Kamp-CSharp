fd33c456b00020a9fbdcd2f34230e6dcd6ca2beb
Sinan Ozturk <sinansinan_@outlook.com.tr>
2023-11-13 13:36:13 +0300

    00P3

---
+++ OOP3/EsnafKrediManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    public class EsnafKrediManager:IKrediManager
+    {
+        public void BiseyYap()
+        {
+
+        }
+        public void Hesapla()
+        {
+            Console.WriteLine("Esnaf Kredisi hesaplama");
+        }
+    }
+}
